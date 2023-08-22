6c81ce0376bc9f4802b71f6023241ee4d905d7eb
dawnai <dawnai@163.com>
2023-08-22 16:17:09 +0800

    v2

---
+++ src/components/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <el-row>
+            <el-col :span="12"><el-card>
+                    <h1>开始设置自己第一个基于VUE的静态网站</h1>
+                </el-card></el-col>
+
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'home'
+}
+</script>
+
+<style></style>
