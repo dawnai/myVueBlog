8d96c4d5942f559ac3e1c6f56ba4cab0fd232332
dawnai <dawnai@163.com>
2023-08-22 20:48:58 +0800

    v5

---
+++ src/components/home/mylottie/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div style="height: 600px; width: 100%;" id="lottie-animation">
+        
+    </div>
+</template>
+
+<script>
+import lottie from 'lottie-web';
+import animationData from '../../../assets/json/animation_llmaqk6p.json';
+export default{
+    name:'mylottie',
+    mounted() {
+    this.initLottieAnimation();
+  },
+  methods: {
+    initLottieAnimation() {
+      const animationContainer = document.getElementById('lottie-animation');
+      const animationOptions = {
+        container: animationContainer,
+        animationData: animationData,
+        loop: true,
+        autoplay: true,
+      };
+
+      this.animationInstance = lottie.loadAnimation(animationOptions);
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
