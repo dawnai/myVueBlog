9d5c3b3e1546031f19b2ce301ecf595fd75ca1ef
dawnai <dawnai@163.com>
2023-08-25 15:53:43 +0800

    v1.0

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -101,7 +101,7 @@
                     <el-row>
                         <el-col :span="12">
                             <h1 style="font-size: 3vw; margin: 10px 0px; color: #002FA7;">简单介绍一下我的网站</h1>
-                            <h1 style="font-size: 2.5vw;">Introduction to the website</h1>
+                            <h1 style="font-size: 2vw;">Introduction to the website</h1>
                             <p class="myWebIntrooduce">
                                 我建立这个网站的目的是分享生活。我希望通过这个平台，能够与您分享我的经验、见解以及我所深爱的艺术。
                             </p>
@@ -132,8 +132,55 @@
                     </el-row>
                 </div>
                 <!-- 内容区域 -->
-                <div>
-                    
+                <div style="margin:30px 100px; border-right:1px solid #bcbbbb ;">
+                    <el-row type="flex"  align="middle">
+                        <el-col :span="8">
+                            <div>
+                                <img src="../../assets/微信图片_20230206145629.jpg" alt="" style="width: 100%;">
+                            </div>
+
+                        </el-col>
+                        <el-col :span="12"  >
+                            <div 
+                                style="background-color: white; padding: 30px 0px 30px 40px;margin-left: -10%;">
+                                <h1 style="font-size: 3vw; margin: 10px 0px; color: #002FA7;">为什么使用这个头像？</h1>
+                                <h1 style="font-size: 2vw;">Logo information</h1>
+                                <p class="myWebIntrooduce">灵感来自村上春树的《ノルウェイの森》(挪威的森林),第九章</p>
+                                <p class="myWebIntrooduce">绿子向渡边彻表达自己爱意的时候说：</p>
+                                <p class="myWebIntrooduce" style="color: #22c358;">"私はあなたが好きで，春の熊が好きなように"</p>
+                                <p class="myWebIntrooduce">"最最喜欢你了，像喜欢春天的熊一样"</p>
+                                <p class="myWebIntrooduceEnglish">"I am fond of you, much like a bear in spring"</p>
+                            </div>
+                        </el-col>
+
+                    </el-row>
+                    <el-row type="flex" justify="center" style="margin-top:30px" gutter="30" >
+                        <el-col  :span="7">
+                            <img src="../../assets/dc7b9bab75eb5c1347536417d7b5a5e.jpg" alt="" style="width: 100%;height:75%">
+                            <h5 style="margin: 10px 0px;color:rgb(59, 59, 59)">CAMERA</h5>
+                            <h3 >华南农业大学的牛牛</h3>
+                            <h5 >_______</h5>
+                            <h5 style="font-style:italic;margin-top:10px">by Web</h5>
+                        </el-col>
+                        <el-col  :span="7">
+                            <img src="../../assets/6fb5314fa7ab822b04926d3d03a01e8.jpg" alt="" style="width: 100%;height:75%">
+                            <h5 style="margin: 10px 0px;color:rgb(59, 59, 59)">PEOPLE</h5>
+                            <a href="https://space.bilibili.com/375065148?spm_id_from=333.337.0.0" style="color:black; text-decoration:none"><h3>B站up:虞兮西西 <svg t="1692949855247" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3799" width="32" height="32"><path d="M455.68 404.032l91.264 529.152c0 0 67.456-69.44 123.136-117.888l122.432 163.904c4.928 6.656 15.68 7.104 23.872 1.088l52.288-38.208c8.256-6.016 10.944-16.32 5.952-22.976l-119.104-159.424c62.208-25.088 164.672-53.632 164.672-53.632L455.68 404.032zM308.352 648.384l-135.872 99.328c-20.544 15.04-24.256 43.968-8 65.408 16.256 21.376 46.272 27.008 66.752 12.032l135.872-99.328c20.992-15.36 24.512-45.504 8.256-66.88C359.168 637.504 329.344 633.024 308.352 648.384zM949.696 238.976c-16.256-21.376-45.632-26.176-67.072-10.496l-134.912 98.688c-21.44 15.68-25.152 44.672-8.896 66.048 16.256 21.376 46.272 27.008 67.712 11.328l134.912-98.688C962.88 290.176 965.952 260.352 949.696 238.976zM319.296 136.832c-15.936-20.928-45.248-25.728-66.752-10.048-20.096 14.72-24.256 43.968-8.32 64.896l105.536 138.816c15.936 20.992 45.696 25.408 65.792 10.688 21.44-15.68 25.216-44.608 9.28-65.6L319.296 136.832zM585.792 301.76c26.176 4.224 50.24-13.376 53.632-39.232l21.184-167.808c3.392-25.792-14.976-49.984-41.536-54.656-26.176-4.224-50.24 13.376-53.632 39.168l-21.248 167.872C540.928 272.96 559.296 297.088 585.792 301.76zM329.728 489.024c2.56-25.92-15.808-50.048-41.536-54.656l-170.048-27.968c-27.072-3.584-50.688 13.696-53.632 39.232-3.904 26.944 14.464 51.072 41.536 54.656l170.048 27.968C301.824 532.736 325.504 515.456 329.728 489.024z" fill="#272636" p-id="3800"></path></svg></h3></a>
+                            
+                            <h5 >_______</h5>
+                            <h5 style="font-style:italic;margin-top:10px">by Bilibili</h5>
+                            
+                        </el-col>
+                        <el-col  :span="7">
+                            <img src="../../assets/f1affa5852406db49844164e1d1381d.jpg" alt="" style="width: 100%;height:75%">
+                            <h5 style="margin: 10px 0px;color:rgb(59, 59, 59)">PEOPLE</h5>
+                            <h3 >很cool的车车</h3>
+                            <h5 >_______</h5>
+                            <h5 style="font-style:italic;margin-top:10px">by NoName</h5>
+                        </el-col>
+
+                    </el-row>
+
                 </div>
 
             </el-main>
@@ -142,12 +189,15 @@
             <el-footer>
                 <div class="myFooter">
                     <el-row :gutter="80">
+                        <!-- 底部左边 -->
                         <el-col :span="12">
-                            <!-- LOGO -->
+
                             <el-row>
+
                                 <el-col :span="24"
                                     style="display: flex;align-items: center;justify-content: space-between;">
-                                    <h1 style="font-size: 32px;">DAWN</h1>
+
+                                    <h1 style="font-size: 32px;">Dawn</h1>
                                     <!-- 微信图标 -->
                                     <a @click="open">
                                         <svg t="1692928493205" class="icon" viewBox="0 0 1024 1024" version="1.1"
@@ -208,7 +258,7 @@
                                 </el-col>
                             </el-row>
                         </el-col>
-
+                        <!-- 底部右边 -->
                         <el-col :span="12">
                             <el-row>
                                 <el-col :span="8">
@@ -357,6 +407,7 @@ h4 {
 }
 
 .myWebIntrooduceEnglish {
+    font-family: "阿里妈妈灵动体 VF Thin";
     color: #202020;
     font-size: small;
 }
