5e751fdb6dc1011ee5a4640655eb366171169d22
dawnai <dawnai@163.com>
2023-09-08 23:07:20 +0800

    设置图像分辨率

--- src/views/AI/index.vue
+++ src/views/AI/index.vue
@@ -2,10 +2,13 @@
     <div style="margin: 30px;">
         <h1>这个模块目前正在开发当中</h1>
         <h1>分享一下最近使用的壁纸：</h1>
+        <h2>为了用户体验，这里将图片分辨率设置为1080p，你可以点击图片以获取4k原图</h2>
         <!-- 大图 -->
         <el-row>
             <el-col :span="24">
-                <img src="@/assets/imgs/AI美少女.jpg" alt="" style="width: 100%;">
+                <a href="https://my-vue-blog-6gpqou7l6bf67715-1300529724.tcloudbaseapp.com/img/AI美少女.d66bc1df.jpg">
+                    <img src="@/assets/imgs/AI美少女1080.jpg" alt="" style="width: 100%;">
+                </a>
             </el-col>
         </el-row>
         <h1>其实我挺抗拒AI生成的图像，觉得那样画出来的东西没有任何灵魂，这一张是我唯一觉得还不错的AI绘图</h1>
