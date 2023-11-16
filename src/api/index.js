22d43e16b1eeba12997998e8ba25d51f73e9697b
dawnai <dawnai@163.com>
2023-11-16 10:06:57 +0800

    更改后端服务器地址

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://dawnai.cloud', // 替换成你的 API 地址
+  baseURL: 'https://www.1ys53p.top', // 替换成你的 API 地址
   timeout: 500000 // 设置请求超时时间
 });
 
